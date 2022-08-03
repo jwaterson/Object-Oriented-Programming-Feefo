39a9fe6521f1786832a91fd40ba65ba965927ccf
Josh Waterson <jjwaterson@hotmail.co.uk>
2022-08-03 17:21:35 +0100

    Added sample tests and skeleton JobStandizer class

---
+++ src/standardizingjobtitle/JobStandardizer.java
@@ -0,0 +1,7 @@
+package standardizingjobtitle;
+
+public class JobStandardizer {
+    public String standardize(String jt) {
+        return "";
+    }
+}
---
+++ src/test/JobStandardizerTest.java
@@ -0,0 +1,33 @@
+package test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import standardizingjobtitle.JobStandardizer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JobStandardizerTest {
+
+    JobStandardizer s;
+
+    @BeforeEach
+    void setUp() {
+        s = new JobStandardizer();
+    }
+
+    @AfterEach
+    void tearDown() {
+        s = null;
+    }
+
+    @Test
+    void sampleCodeTest() {
+        assertAll(
+                () -> assertEquals("Software engineer", s.standardize("Java engineer")),
+                () -> assertEquals("Software engineer", s.standardize("C# engineer")),
+                () -> assertEquals("Accountant", s.standardize("Accountant")),
+                () -> assertEquals("Accountant", s.standardize("Chief Accountant"))
+        );
+    }
+}
