ba915148c1bd5f0d0e0a9157094bb0c7388feca7
Josh Waterson <jjwaterson@hotmail.co.uk>
2022-08-04 09:56:19 +0100

    Added ArrayStats files

---
+++ src/numberstatistics/ArrayStats.java
@@ -0,0 +1,4 @@
+package numberstatistics;
+
+public class ArrayStats {
+}
---
+++ src/test/ArrayStatsTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class ArrayStatsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
